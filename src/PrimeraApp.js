f4e2e1a7d20d39e7dfa3d69caa6854fc473ef583
Lufe1895 <luislecompt@gmail.com>
2021-09-19 20:07:15 -0500

    Fin seccion 4

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,13 +1,23 @@
-const PrimeraApp = ({ saludo = 'Hola, mundo.'}) => {
+import PropTypes from 'prop-types';
+
+const PrimeraApp = ({ saludo, subtitulo }) => {
 
     return (
         <>
             <h1>{ saludo }</h1>
             {/* <pre>{ JSON.stringify(saludo, null, 3) }</pre> */}
-            <p>Mi primera aplicación.</p>
+            <p>{ subtitulo }</p>
         </>
     );
     
+}
+
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired,
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtítulo.',
 }
 
 export default PrimeraApp;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import ReactDOM from "react-dom";
-import PrimeraApp from "./PrimeraApp";
+// import PrimeraApp from "./PrimeraApp";
+import CounterApp from './CounterApp';
 
 import './index.css';
 
 const divRoot = document.querySelector('#app');
 
-ReactDOM.render(<PrimeraApp saludo="Hola, soy Luis." />, divRoot);
+// ReactDOM.render(<PrimeraApp saludo="Hola, soy Luis." />, divRoot);
+
+ReactDOM.render(<CounterApp value={ 123 } />, divRoot)
